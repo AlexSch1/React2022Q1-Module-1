cc9138972eb58b34aa497d0a94419cda2b4cf0cc
Alex <al.shulyak@icloud.com>
2023-05-03 18:51:09 +0300

    feat: add about us page

---
+++ src/pages/AboutUs.tsx
@@ -0,0 +1,17 @@
+import './AboutUs.scss';
+
+export const AboutUs = () => {
+  return (
+    <div className="">
+      <div className="about-us__container">
+        <div className="about-us__col">
+          <div className="space"></div>
+        </div>
+        <div className="about-us__col">
+          <h1>About as</h1>
+          <p> The page about us. </p>
+        </div>
+      </div>
+    </div>
+  );
+};
